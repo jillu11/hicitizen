3b34ad0c4425c999211f72030433523d15c60b10
Santhosh <santhosh@openshelf.xyz>
2024-12-31 22:45:52 +0100

    fixed build issues

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,30 +1,29 @@
-import '../styles/globals.css';
-import { Footer } from '../components/footer';
-import { Header } from '../components/header';
+import { Inter, Roboto, Open_Sans } from 'next/font/google'
 
-export const metadata = {
-    title: {
-        template: '%s | HiCitizen Academy',
-        default: 'HiCitizen Academy - Your Path to English Fluency'
-    }
-};
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+})
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
+})
 
 export default function RootLayout({ children }) {
-    return (
-        <html lang="en" data-theme="light">
-            <head>
-                <link rel="icon" href="/favicon.svg" sizes="any" />
-                <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
-            </head>
-            <body className="antialiased text-gray-800">
-                <div className="flex flex-col min-h-screen">
-                    <div className="flex flex-col w-full mx-auto grow">
-                        <Header />
-                        <div className="grow">{children}</div>
-                        <Footer />
-                    </div>
-                </div>
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en" className={`${inter.variable} ${roboto.variable}`}>
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
+
+export const metadata = {
+  title: 'HiCitizen Academy',
+  description: 'Your pathway to success',
 }
